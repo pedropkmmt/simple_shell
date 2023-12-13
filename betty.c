8535fdc71a41a0e078dfd6c4e3207df974960322
pedro <pedropkmmt@gmail.com>
2023-12-13 12:11:04 -0800

    try

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main is the main function
+ * @void: void
+ * return: return 0
+ */
+
+int main(void)
+{
+	printf("Betty code is clean\n");
+	return (0);
+}
