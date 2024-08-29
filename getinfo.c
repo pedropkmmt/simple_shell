a90d2a95c9f0d9f4fe3b7cd486bfd490c96cff19
root <you@example.com>
2024-08-28 18:02:21 -0700

    update

--- getinfo.c
+++ getinfo.c
@@ -39,8 +39,7 @@ void set_info(info_t *info, char **av)
 			;
 		info->argc = i;
 
-		replace_alias(info);
-		replace_vars(info);
+		
 	}
 }
 
@@ -56,7 +55,7 @@ void free_info(info_t *info, int all)
 	info->path = NULL;
 	if (all)
 	{
-		if (!info->cmd_buf)
+           if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
 			free_list(&(info->env));
